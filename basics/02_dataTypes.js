34077852589b25600e99e3f1e9eb9b9ffb770657
NegiKareena <159912897+NegiKareena@users.noreply.github.com>
2024-04-16 21:26:33 +0000

    datatypes

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"Use strict ";// (not necessary to write) by writing this it is treated as newer version of javascript automaticallyit treated as 
+//alert(4+4)//  we are using nodejs , so can't use this , can be use in browser
+console.log(3+3) //code readiablity should be there 
+//ecmaStrict  standards : defines javascripts standards 
+// Two dcumentation TC 39 and mdn 
+//Datatype
+ let name ="Kareena "// string =>""
+ let age = 18 // number => 2 to power 53
+ //bigint
+ let areyou= true// boolean = true/false
+ null //(null is an object(type of null ) where as undefined is undefined ) standalone value it means that it's empty undefined means nothing is assigned 
+ //symbol => for uniqueness
+ //object 
+ console.log(typeof "Kareena");
+
+
+ 
+ 
+
